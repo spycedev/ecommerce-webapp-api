e0f13908684aa4b183a33b787c9f168fa43dbcff
Rugie Ann Barrameda <norugie015@gmail.com>
2023-11-04 20:22:01 -0700

    add products and get products function

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const db = mysql.createConnection({
 });
 
 // Route for creating products
-app.post('/product/create', (request, response) => {
+app.post('/products/create', (request, response) => {
     const name = request.body.name;
     const description = request.body.description;
     const price = request.body.price;
@@ -24,13 +24,18 @@ app.post('/product/create', (request, response) => {
     const image = request.body.image;
 
     db.query('INSERT INTO products (name, description, price, quantity, image) VALUES (?, ?, ?, ?, ?)', [name, description, price, quantity, image], (error, result) => {
-        if (error) {
-            console.log(error);
-        } else {
-            response.send('New product created.');
-        }
+        if (error) console.log(error);
+        else response.send('New product created.');
     });
 });
+
+app.get('/products', (request, response) => {
+    db.query('SELECT * FROM products', (error, result) => {
+        if (error) console.log(error);
+        else response.send(result);
+    });
+});
+
 
 app.listen(3001, () => {
     console.log('Server is running on port 3001');
