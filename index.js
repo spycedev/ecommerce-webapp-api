317dad0153d6ab8aab8661068bfc1bf9c36c27f2
Rugie Ann Barrameda <42895277+norugie@users.noreply.github.com>
2023-11-04 17:34:43 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Database variable
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: '',
+    database: 'ecommerce_webapp_api'
+});
+
+// Route for creating products
+app.post('/product/create', (request, response) => {
+    const name = request.body.name;
+    const description = request.body.description;
+    const price = request.body.price;
+    const quantity = request.body.quantity;
+    const image = request.body.image;
+
+    db.query('INSERT INTO products (name, description, price, quantity, image) VALUES (?, ?, ?, ?, ?)', [name, description, price, quantity, image], (error, result) => {
+        if (error) {
+            console.log(error);
+        } else {
+            response.send('New product created.');
+        }
+    });
+});
+
+app.listen(3001, () => {
+    console.log('Server is running on port 3001');
+});
