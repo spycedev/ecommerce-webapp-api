b9f63eb1199153df0d48c75c3f9255f9cdc05179
Rugie Ann Barrameda <norugie015@gmail.com>
2023-11-05 22:10:32 -0800

    added delete route

--- index.js
+++ index.js
@@ -15,7 +15,25 @@ const db = mysql.createConnection({
     database: 'ecommerce_webapp_api'
 });
 
-// Route for creating products
+// Route for getting all products
+app.get('/products', (request, response) => {
+    db.query('SELECT * FROM products', (error, result) => {
+        if (error) console.log(error);
+        else response.send(result);
+    });
+});
+
+// Route for getting a product
+app.get('/products/:id', (request, response) => {
+    const id = request.params;
+    console.log(id);
+    db.query('SELECT * FROM products WHERE id = ?', id, (error, result) => {
+        if (error) console.log(error);
+        else response.send(result);
+    });
+});
+
+// Route for creating a product
 app.post('/products/create', (request, response) => {
     const name = request.body.name;
     const description = request.body.description;
@@ -29,13 +47,31 @@ app.post('/products/create', (request, response) => {
     });
 });
 
-app.get('/products', (request, response) => {
-    db.query('SELECT * FROM products', (error, result) => {
+// Route for updating a product
+app.put('/products/update', (request, response) => {
+    const id = request.body.id;
+    const name = request.body.name;
+    const description = request.body.description;
+    const price = request.body.price;
+    const quantity = request.body.quantity;
+    const image = request.body.image;
+
+    db.query('UPDATE products SET name = ?, description = ?, price = ?, quantity = ?, image = ? WHERE id = ?', [name, description, price, quantity, image, id], (error, result) => {
+        if (error) console.log(error);
+        else response.send('Product has been updated.');
+    });
+});
+
+// Route for deleting a product
+app.delete('/products/:id/delete', (request, response) => {
+    const id = request.params.id;
+
+    console.log(id);
+    db.query('DELETE FROM products WHERE id = ?', id, (error, result) => {
         if (error) console.log(error);
         else response.send(result);
     });
 });
-
 
 app.listen(3001, () => {
     console.log('Server is running on port 3001');
